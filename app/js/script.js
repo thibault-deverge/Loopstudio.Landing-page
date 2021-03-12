50ab82d1c8f46241ad646f893983085d2a2bbc8d
thibault <thibault.deverge@gmail.com>
2021-03-12 14:29:20 +0700

    setup hamburger menu

---
+++ app/js/script.js
@@ -0,0 +1,7 @@
+const btnHamburger = document.getElementById("btnHamburger");
+const headerMenu = document.querySelector(".header__menu");
+const overlay = document.querySelector("overlay");
+const fadeElems = document.querySelectorAll(".had-fade");
+
+///////////////////////
+// Helper function Open and Close menu
